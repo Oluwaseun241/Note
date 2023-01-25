aca75b26a3dd38c543389704ce687ad7c5c41349
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2023-01-25 20:25:23 +0100

    testing: LFG

--- main.py
+++ main.py
@@ -4,25 +4,25 @@ import sys
 def create_note(content):
     url = 'https://63a52acf821953d4f2c41d5e.mockapi.io/api/v1/notes'
     data = {"content": content}
-    response = request.post(url, json=data)
+    response = requests.post(url, json=data)
     print(response.json())
 
 def fetch_notes():
     url = 'https://63a52acf821953d4f2c41d5e.mockapi.io/api/v1/notes'
-    response = request.get(url)
+    response = requests.get(url)
     print(response.json())
 
     
 def delete_note(id):
     url = f'https://63a52acf821953d4f2c41d5e.mockapi.io/api/v1/notes/{note_id}'
-    response = request.delete(url)
+    response = requests.delete(url)
     print(response.json())
 
 
 def update_note(id, content):
     url = f'https://63a52acf821953d4f2c41d5e.mockapi.io/api/v1/notes/{note_id}'
     data = {"content": content}
-    response = request.put(url, json=data)
+    response = requests.put(url, json=data)
     print(response.json())
 
 if __name__ == "__main__":
