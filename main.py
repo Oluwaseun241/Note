9226accaf9cf5e975f41b01344cec916f9362ea9
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2023-01-25 20:15:49 +0100

    testing: api

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import request
+import sys
 
 def create_note(content):
     url = 'https://63a52acf821953d4f2c41d5e.mockapi.io/api/v1/notes'
@@ -7,9 +8,24 @@ def create_note(content):
     print(response.json())
 
 def fetch_notes():
+    url = 'https://63a52acf821953d4f2c41d5e.mockapi.io/api/v1/notes'
+    response = request.get(url)
+    print(response.json())
 
     
 def delete_note(id):
 
 
 def update_note(id, content)
+
+if if __name__ == "__main__":
+    if sys.argv[1] == "create-note":
+        create_note(sys.argv[2])
+    elif sys.argv[1] == "fetch-notes":
+        fetch_notes()
+    elif sys.argv[1] == "delete-note":
+        delete_note(sys.argv[2])
+    elif sys.argv[1] == "update-note":
+        update_note(sys.argv[2], sys.argv[3])
+    else:
+        print("Invalid command.\n Use 'create-note', 'fetch-notes', 'delete-note', or 'update-note'.")
