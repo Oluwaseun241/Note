3075a6452b5efefc42092c3ac25e3be8846de063
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2023-01-25 20:24:27 +0100

    requests:God abeg

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import request
+import requests
 import sys
 
 def create_note(content):
@@ -14,11 +14,18 @@ def fetch_notes():
 
     
 def delete_note(id):
+    url = f'https://63a52acf821953d4f2c41d5e.mockapi.io/api/v1/notes/{note_id}'
+    response = request.delete(url)
+    print(response.json())
 
 
-def update_note(id, content)
+def update_note(id, content):
+    url = f'https://63a52acf821953d4f2c41d5e.mockapi.io/api/v1/notes/{note_id}'
+    data = {"content": content}
+    response = request.put(url, json=data)
+    print(response.json())
 
-if if __name__ == "__main__":
+if __name__ == "__main__":
     if sys.argv[1] == "create-note":
         create_note(sys.argv[2])
     elif sys.argv[1] == "fetch-notes":
