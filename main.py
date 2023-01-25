886cc476d29a52cfa49ce8d8208b097419d1d1e4
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2023-01-25 19:35:32 +0100

    request: create note

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import request
 
+def create_note(content):
+    url = 'https://63a52acf821953d4f2c41d5e.mockapi.io/api/v1/notes'
+    data = {"content": content}
+    response = request.post(url, json=data)
+    print(response.json())
 
+def fetch_notes():
+
+    
+def delete_note(id):
+
+
+def update_note(id, content)
